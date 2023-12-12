50f6ebdff02151bbce7953f00cfe231dcae13328
r0han99 <r409067@gmail.com>
2023-12-12 21:41:37 +0530

    updates

--- pages/ImageToDescription.py
+++ pages/ImageToDescription.py
@@ -2,24 +2,28 @@ import pandas as pd
 from transformers import pipeline
 import streamlit as st 
 
-
 def load_pipeline():
-
-    # Create the pipeline for image-to-text
-    pipe = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning", from_pt=True)
-    return pipe
+    try:
+        # Create the pipeline for image-to-text
+        pipe = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+        return pipe
+    except Exception as e:
+        st.error(f"Failed to load the pipeline: {e}")
+        return None
 
 st.set_page_config(layout='wide', page_icon="🧑🏻‍🎨")
 
 st.title("Artwork Description Generator")
 pipe = load_pipeline()
 
-image = st.file_uploader("Upload an Artwork")
-image_place = st.empty()
+if pipe:
+    image = st.file_uploader("Upload an Artwork")
+    image_place = st.empty()
 
-if image is not None:
-    image_place.image(image, use_column_width=True)
-    captions = pipe(image)
-    image_place.image(image, use_column_width=True, caption=f"{captions[0]['generated_text']}")
-
-
+    if image is not None:
+        image_place.image(image, use_column_width=True)
+        try:
+            captions = pipe(image)
+            image_place.image(image, use_column_width=True, caption=f"{captions[0]['generated_text']}")
+        except Exception as e:
+            st.error(f"Error in generating caption: {e}")
