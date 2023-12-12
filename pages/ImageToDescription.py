9743fc27ca0f95670a0706a669f4b10e8f13057f
r0han99 <r409067@gmail.com>
2023-12-12 21:37:21 +0530

    updates

--- pages/ImageToDescription.py
+++ pages/ImageToDescription.py
@@ -7,21 +7,19 @@ def load_pipeline():
 
     # Create the pipeline for image-to-text
     pipe = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning", from_pt=True)
-
     return pipe
 
-
-st.set_page_config(layout='wide', page_icon="🧑🏻‍🎨", )
-
+st.set_page_config(layout='wide', page_icon="🧑🏻‍🎨")
 
 st.title("Artwork Description Generator")
 pipe = load_pipeline()
 
 image = st.file_uploader("Upload an Artwork")
 image_place = st.empty()
-image_place.image(image, use_column_width=True)
 
-captions = pipe(image)
-image_place.image(image, use_column_width=True, caption=f"{captions[0]['generated_text']}")
+if image is not None:
+    image_place.image(image, use_column_width=True)
+    captions = pipe(image)
+    image_place.image(image, use_column_width=True, caption=f"{captions[0]['generated_text']}")
 
 
